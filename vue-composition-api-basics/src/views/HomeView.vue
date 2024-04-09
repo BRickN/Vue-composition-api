1f55a27b544a4dc54846403a83b739ec14990014
Rick Nieling <b.nieling@vgz.nl>
2024-04-09 21:03:14 +0200

    beginning

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+interface CounterData {
+  count: number
+  counterTitle: string
+}
+const count = ref<number>(0)
+const counterTitle = ref<string>('My counter')
+
+const counterData: CounterData = reactive({
+  count: 0,
+  counterTitle: 'myTitle'
+})
+
+const increment = () => {
+  counterData.count++
+}
+const decrement = () => {
+  counterData.count--
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h3>{{ counterData.counterTitle }}</h3>
+    <div>
+      <button class="btn" @click.prevent="decrement">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button class="btn" @click.prevent="increment">+</button>
+    </div>
+    <div>
+      <h4>Edit</h4>
+      <input type="text" v-model="counterData.counterTitle" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 50vh;
+}
+</style>
